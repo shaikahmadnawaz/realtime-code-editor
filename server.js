5725a59b01f79f0028d63928fca21bec6d7ec79d
Shaik Ahamd Nawaz <nawazcode@gmail.com>
2022-09-10 23:21:31 +0530

    ws error handling

--- server.js
+++ server.js
@@ -5,13 +5,40 @@ const app = express();
 const http = require("http");
 // importing socket
 const { Server } = require("socket.io");
-
+const ACTIONS = require("./src/Actions");
 const server = http.createServer(app);
 // creating instance for server class
 const io = new Server(server);
+const userSocketMap = {};
+function getAllConnectedClients(roomId) {
+  // Map
+  // this will return the rooId in all rooms
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      };
+    }
+  );
+}
 // This event is triggred when any socket is Connected
 io.on("connection", (socket) => {
   console.log("socket connected", socket.id);
+  // server listening that is emitted by client
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    // if already user present and getting new ids also
+    const clients = getAllConnectedClients(roomId);
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit(ACTIONS.JOINED, {
+        clients,
+        username,
+        socketId: socket.id,
+      });
+    });
+  });
 });
 
 // listening on 5000 port
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,31 +1,66 @@
 import React, { useState, useRef, useEffect } from "react";
+import toast from "react-hot-toast";
+import ACTIONS from "../Actions";
 import Client from "../components/Client";
 import Editor from "../components/Editor";
 import { initSocket } from "../socket";
-import { useLocation } from "react-router-dom";
-import ACTIONS from "../Actions";
+import {
+  useLocation,
+  useNavigate,
+  Navigate,
+  useParams,
+} from "react-router-dom";
 const EditorPage = () => {
   // initialisation of sockets
   /* useRef is used to store data that is used for multiple rendering 
   so that data will not change by re - rendering, even re-rendering also will not happen */
   const socketRef = useRef(null);
   const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+
+  const [clients, setClients] = useState([]);
   // whenever the page re-renders the use rffect is called
   useEffect(() => {
     const init = async () => {
       // By calling this client/browser will connect with server
       socketRef.current = await initSocket();
+      // We have to send error message to client if there is problem in connection
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed, try again later.");
+        // if we get error we redirect to home page
+        reactNavigator("/");
+      }
+
       // We have to send message to the sever that join me
-      // socketRef.current.emit(ACTIONS.JOIN, {
-      //   roomId,
-      //   // location is used to get username from state
-      //   //  ? this will not give error if we did username
-      //   username: location.state?.username,
-      // });
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        // location is used to get username from state
+        //  ? this will not give error if we did username
+        username: location.state?.username,
+      });
+      // Listening for joined event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClients(clients);
+        }
+      );
     };
     init();
   }, []);
-  const [clients, setClients] = useState([{ socketId: 1, username: "nawaz" }]);
+  // if we didnt get any state then navigate to home
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div className="mainWrap">
       <div className="aside">
