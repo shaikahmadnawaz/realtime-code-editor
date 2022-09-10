0143baa04060b3ae847ef08f916d65315aeb4907
Shaik Ahamd Nawaz <nawazcode@gmail.com>
2022-09-10 17:58:10 +0530

    Editor design

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,26 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import Codemirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
 const Editor = () => {
-  return <div>Editor goes here</div>;
+  useEffect(() => {
+    async function init() {
+      //   These are methods present in code mirror package
+      Codemirror.fromTextArea(document.getElementById("realtimeEditor"), {
+        //   To enable this we have to import some css properties
+        mode: { name: "javascript", json: true },
+        theme: "dracula",
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      });
+    }
+    init();
+  }, []);
+  return <textarea id="realtimeEditor"></textarea>;
 };
 
 export default Editor;
