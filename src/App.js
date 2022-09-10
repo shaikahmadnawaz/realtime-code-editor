6e1d259cb509c2bcad3c584fa408aa4f31669510
Shaik Ahamd Nawaz <nawazcode@gmail.com>
2022-09-10 14:40:37 +0530

    Home page done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Toaster } from "react-hot-toast";
 
+import Home from "./pages/Home";
+import EditorPage from "./pages/EditorPage";
+/* React Router DOM is an npm package that enables you to implement dynamic routing 
+  in a web app. It allows you to display pages and allow users to navigate them. 
+  It is a fully-featured client and server-side routing library for React. */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /* What is a fragment in React? (<></>)
+      React Fragments allow you to wrap or group multiple elements 
+      without adding an extra node to the DOM. 
+      This can be useful when rendering multiple child elements/components 
+      in a single parent component.*/
+    /* What are props in React? 
+      We use props in React to pass data from one component to another 
+      (from a parent component to a child component(s)). 
+      Props is just a shorter way of saying properties. 
+      They are useful when you want the flow of data in your app to be dynamic. */
+    <>
+      <div>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            success: {
+              theme: {
+                primary: "#4aed88",
+              },
+            },
+          }}
+        ></Toaster>
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/editor/:roomId" element={<EditorPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+const Home = () => {
+  const navigate = useNavigate();
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+    toast.success("Created a new room");
+  };
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      toast.error("ROOM ID & username is required");
+      return;
+    }
+    // Redirect
+    navigate(`/editor/${roomId}`, {
+      // passing data from one page to another page using state
+      // sending username to editor page
+      state: {
+        username,
+      },
+    });
+  };
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+          <button className="btn joinBtn" onClick={joinRoom}>
+            Join
+          </button>
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <a onClick={createNewRoom} href="" className="createNewBtn">
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Built with 💛 &nbsp; by &nbsp;
+          <a href="https://github.com/sanawaz1080">Shaik Ahmad Nawaz</a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
